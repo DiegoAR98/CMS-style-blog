ee54033b2a61249b7a591f20031a81d6219914d8
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-02-05 12:32:58 -0500

    Fixed bug on logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,28 +1,14 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
   
-    // Collect values from the login form
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-    console.log(username, password);
-  
-    if (username && password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
   };
   
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+  document.querySelector('#logout').addEventListener('click', logout);
